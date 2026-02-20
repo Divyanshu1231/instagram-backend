169e1ae70d2de61650e4dc3c6b5046720062788c
Divyanshu1231 <divyanshusrivastava1021@gmail.com>
2026-02-20 11:39:48 +0530

    Fixed port for Render

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const JWT_SECRET = "secretkey123";
 
 // ===== MongoDB connection =====
